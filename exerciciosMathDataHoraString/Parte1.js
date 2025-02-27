f2ee462ddf90413e40d489540a72a4b877ecc830
MiguelMPicinin <miguelpicinin6@gmail.com>
2025-02-27 07:49:59 -0300

    20.02.2025

---
+++ exerciciosMathDataHoraString/Parte1.js
@@ -0,0 +1,13 @@
+
+
+//exercicio 1
+let randomNum = Math.floor(Math.random()*100) +1;
+console.log(randomNum);
+
+//exercicio 2
+let num = Number(prompt("Digite um numero"))
+function raizQuadrada(num) {
+    return Math.sqrt(num);
+};
+
+//
